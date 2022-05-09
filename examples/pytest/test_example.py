1356e28af5de482912ae977c14d8a800544a99fc
jack89roberts <jroberts@turing.ac.uk>
2022-05-09 11:34:15 +0100

    pytest examples

---
+++ examples/pytest/test_example.py
@@ -0,0 +1,11 @@
+"""Example tests to show the VS Code test explorer"""
+
+
+def test_good():
+    """A test that passes"""
+    assert 2 > 1
+
+
+def test_bad():
+    """A test that fails"""
+    assert 1 == 2
