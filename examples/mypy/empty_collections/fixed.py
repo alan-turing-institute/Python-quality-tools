aff371c33196cf5f1c7ff2d7b1356beb496c0e8b
Jim Madge <jim@jmadge.com>
2022-05-10 09:08:02 +0100

    Add mypy empty collections example

---
+++ examples/mypy/empty_collections/fixed.py
@@ -0,0 +1,5 @@
+# Untyped collections
+# Adapted from the mypy documentation
+# https://mypy.readthedocs.io/en/stable/type_inference_and_annotations.html#explicit-types-for-collections
+li: list[int] = []
+di: dict[str, int] = {}
